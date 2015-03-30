319d996179adc9acb0bea7898202b24bd6163e8c
mattya <e.mattya@gmail.com>
2015-03-31 07:03:16 +0900

    minor

--- build.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo "Fetch mshadow..."
-git clone https://github.com/tqchen/mshadow.git -b master
-echo "Fetch picojson..."
-git clone https://github.com/kazuho/picojson.git -b master
-
